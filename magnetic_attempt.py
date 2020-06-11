212d179c548cd7de3241e1ce89a374c52990b2be
cacsphysics <ccartagena@brynmawr.edu>
2020-06-11 13:08:01 -0600

    Increasing readability
    I added various spacing to increase code readability.

--- magnetic_attempt.py
+++ magnetic_attempt.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jan  8 16:09:16 2018
-This code is an attempt to simulate my current coil configuration.
+This code  simulates my the stuffing coil configuration at BMPL.
 @author: ccartagena
 """
 
@@ -17,7 +17,7 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 from magnetics import compute_greens, compute_greens_mp
 
 # Define useful function to plot fields
-def plot_fields(R,Z,psi,BR,BZ):
+def plot_fields(R, Z, psi, BR, BZ):
     # Plot the fields!
     """
     
@@ -26,7 +26,7 @@ def plot_fields(R,Z,psi,BR,BZ):
     contain the three subplots of the huge object, fig.
     
     """
-    fig,(ax1,ax2,ax3) = plt.subplots(1,3,figsize=(16,9))
+    fig,(ax1, ax2, ax3) = plt.subplots(1, 3, figsize = (16, 9))
     
     """
     
@@ -35,7 +35,7 @@ def plot_fields(R,Z,psi,BR,BZ):
     
     """
     div1 = make_axes_locatable(ax1)
-    cax1 = div1.append_axes("right","5%",pad="3%")
+    cax1 = div1.append_axes("right", "5%", pad = "3%")
     
     """
     
@@ -50,49 +50,49 @@ def plot_fields(R,Z,psi,BR,BZ):
     
     """
 
-    ax1.contour(R,Z,psi,50,colors="k")
-    cf1 = ax1.contourf(R,Z,np.sqrt(BR**2+BZ**2),locator=ticker.LogLocator())
-    cbar1 = fig.colorbar(cf1,cax=cax1)
+    ax1.contour(R, Z, psi, 50, colors="k")
+    cf1 = ax1.contourf(R, Z, np.sqrt(BR**2 + BZ**2), locator = ticker.LogLocator())
+    cbar1 = fig.colorbar(cf1, cax = cax1)
     cbar1.set_label("|B| (T)")
     ax1.set_aspect(1)
     ax1.set_xlabel("R (m)")
     ax1.set_ylabel("Z (m)")
     # The two lines below need to be changed when the radial and axial 
     # normalizations change.
-    ax1.plot([0.247,0.247],[-1,1],'-',color = 'white',linewidth = 2)
-    ax1.plot([0.617,0.617],[-1,1],'-',color = 'white',linewidth = 2)
+    ax1.plot([0.247, 0.247], [-1, 1],'-',color = 'white',linewidth = 2)
+    ax1.plot([0.617, 0.617], [-1, 1],'-',color = 'white',linewidth = 2)
     """
     The next three sets of commands follow the same logic as the first.
     """
     div2 = make_axes_locatable(ax2)
-    cax2 = div2.append_axes("right","5%",pad="3%")
+    cax2 = div2.append_axes("right", "5%", pad = "3%")
     
-    ax2.contour(R,Z,psi,50,colors="k")
-    cf2 = ax2.contourf(R,Z,np.abs(BR),locator=ticker.LogLocator())
-    cbar2 = fig.colorbar(cf2,cax=cax2)
+    ax2.contour(R, Z, psi, 50, colors = "k")
+    cf2 = ax2.contourf(R, Z, np.abs(BR), locator = ticker.LogLocator())
+    cbar2 = fig.colorbar(cf2, cax = cax2)
     cbar2.set_label("|B_R| (T)") # I need to change the white space.
     ax2.set_aspect(1)
     ax2.set_xlabel("R (m)")
  #   ax2.set_ylabel("Z (m)")
     # The two lines below need to be changed when the radial and axial 
     # normalizations change.
-    ax2.plot([0.247,0.247],[-1,1],'-',color = 'white',linewidth = 2)
-    ax2.plot([0.617,0.617],[-1,1],'-',color = 'white',linewidth = 2)
+    ax2.plot([0.247, 0.247], [-1, 1], '-', color = 'white',linewidth = 2)
+    ax2.plot([0.617, 0.617], [-1, 1], '-', color = 'white',linewidth = 2)
     
     div3 = make_axes_locatable(ax3)
-    cax3 = div3.append_axes("right","5%",pad="3%")
+    cax3 = div3.append_axes("right", "5%", pad = "3%")
     
-    cf3 = ax3.contourf(R,Z,np.abs(BZ),locator=ticker.LogLocator())
-    ax3.contour(R,Z,psi,50,colors="k")
-    cbar3 = fig.colorbar(cf3,cax=cax3)
+    cf3 = ax3.contourf(R, Z, np.abs(BZ), locator = ticker.LogLocator())
+    ax3.contour(R, Z, psi, 50, colors="k")
+    cbar3 = fig.colorbar(cf3, cax = cax3)
     cbar3.set_label("|B_Z| (T)")
     ax3.set_aspect(1)
     ax3.set_xlabel("R (m)")
 #    ax3.set_ylabel("Z (m)")
     # The two lines below need to be changed when the radial and axial 
     # normalizations change.
-    ax3.plot([0.247,0.247],[-1,1],'-',color = 'white',linewidth = 2)
-    ax3.plot([0.617,0.617],[-1,1],'-',color = 'white',linewidth = 2)
+    ax3.plot([0.247, 0.247], [-1, 1], '-', color = 'white',linewidth = 2)
+    ax3.plot([0.617, 0.617], [-1, 1], '-', color = 'white',linewidth = 2)
     
     plt.tight_layout()
     plt.savefig('OrientationIIv4.pdf')
@@ -105,9 +105,9 @@ def plot_fields(R,Z,psi,BR,BZ):
 # calculate the magnetic flux and field)
 m = 1001
 n = 2001
-r = np.linspace(0,1,m)
-z = np.linspace(-1,1,n)
-R,Z = np.meshgrid(r,z)
+r = np.linspace(0, 1, m)
+z = np.linspace(-1, 1, n)
+R,Z = np.meshgrid(r, z)
 
 #################################################################
 """
@@ -138,16 +138,16 @@ BZ1 = (gBZ1.dot(coil_currents1)).reshape(R.shape)
 h1 = 20
 w1 = 5
 n_coils1 = h1*w1
-rlocs1 = np.linspace(0.948,1,w1)
-zlocs1 = np.linspace(-0.025,0.228,h1)
-rlocs1,zlocs1 = np.meshgrid(rlocs1,zlocs1)
+rlocs1 = np.linspace(0.948, 1, w1)
+zlocs1 = np.linspace(-0.025, 0.228, h1)
+rlocs1, zlocs1 = np.meshgrid(rlocs1, zlocs1)
 rlocs1 = rlocs1.flatten()
 zlocs1 = zlocs1.flatten()
 cur_dir1 = np.ones(n_coils1)
 # zip current locations and directions into a list of 3-tuples (needed
 # for compute_greens function)
-rzdir1 = zip(rlocs1,zlocs1,cur_dir1)
-gpsi1, gBR1, gBZ1 = compute_greens(R.flatten(),Z.flatten(),rzdir1)
+rzdir1 = zip(rlocs1, zlocs1, cur_dir1)
+gpsi1, gBR1, gBZ1 = compute_greens(R.flatten(), Z.flatten(), rzdir1)
 
 coil_currents1 = +180*np.ones(n_coils1)
 
@@ -185,16 +185,16 @@ BZ2 = (gBZ2.dot(coil_currents2)).reshape(R.shape)
 h2 = 20
 w2 = 5
 n_coils2 = h2*w2
-rlocs2 = np.linspace(0.948,1,w2)
-zlocs2 = np.linspace(0.506,0.759,h2)
-rlocs2,zlocs2 = np.meshgrid(rlocs2,zlocs2)
+rlocs2 = np.linspace(0.948, 1, w2)
+zlocs2 = np.linspace(0.506, 0.759, h2)
+rlocs2, zlocs2 = np.meshgrid(rlocs2, zlocs2)
 rlocs2 = rlocs2.flatten()
 zlocs2 = zlocs2.flatten()
 cur_dir2 = np.ones(n_coils2)
 # zip current locations and directions into a list of 3-tuples (needed
 # for compute_greens function)
-rzdir2 = zip(rlocs2,zlocs2,cur_dir2)
-gpsi2, gBR2, gBZ2 = compute_greens(R.flatten(),Z.flatten(),rzdir2)
+rzdir2 = zip(rlocs2, zlocs2, cur_dir2)
+gpsi2, gBR2, gBZ2 = compute_greens(R.flatten(), Z.flatten(), rzdir2)
 
 coil_currents2 = -180*np.ones(n_coils2)
 
@@ -230,16 +230,16 @@ BZ3 = (gBZ3.dot(coil_currents3)).reshape(R.shape)
 h3 = 25
 w3 = 4
 n_coils3 = h3*w3
-rlocs3 = np.linspace(0.152,.167,w3)
-zlocs3 = np.linspace(0.038,0.578,h3)
-rlocs3,zlocs3 = np.meshgrid(rlocs3,zlocs3)
+rlocs3 = np.linspace(0.152, 0.167, w3)
+zlocs3 = np.linspace(0.038, 0.578, h3)
+rlocs3, zlocs3 = np.meshgrid(rlocs3, zlocs3)
 rlocs3 = rlocs3.flatten()
 zlocs3 = zlocs3.flatten()
 cur_dir3 = np.ones(n_coils3)
 # zip current locations and directions into a list of 3-tuples (needed
 # for compute_greens function)
-rzdir3 = zip(rlocs3,zlocs3,cur_dir3)
-gpsi3, gBR3, gBZ3 = compute_greens(R.flatten(),Z.flatten(),rzdir3)
+rzdir3 = zip(rlocs3, zlocs3, cur_dir3)
+gpsi3, gBR3, gBZ3 = compute_greens(R.flatten(), Z.flatten(), rzdir3)
 
 coil_currents3 = +2800*np.ones(n_coils3)
 
@@ -259,7 +259,7 @@ BZ = BZ1 + BZ2 + BZ3
 psi = psi1 + psi2 + psi3
 
 # Note: All quantities are in SI units
-plot_fields(R,Z,psi,BR,BZ)
+plot_fields(R, Z, psi, BR, BZ)
 #################################################################
 """
 #################################################################
